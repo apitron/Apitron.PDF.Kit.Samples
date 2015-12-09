139a336415ae61f2b6078cc949c7d19f72de11d2
apitron <sales@apitron.com>
2015-12-09 10:48:19 +0400

    Sample showing how to check whether the document conforms to PDFA standard

---
+++ PDFACheckingSample/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Apitron.PDF.Kit;
+
+namespace PDFACheckingSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Stream fileStream = File.Open("../../Data/document_pdfa.pdf",FileMode.Open))
+            {
+                using (FixedDocument doc = new FixedDocument(fileStream))
+                {
+                    // PdfStandard property can be used to check whether the document uses PDFA standard.
+                    // It will return PDfStandard.Default if the doc doesn't use PDFA or PdfStandard.PDFA if it does.
+                    Console.WriteLine(string.Format("Document uses '{0}' standard",Enum.GetName(typeof(PdfStandard),doc.PdfStandard)));
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
