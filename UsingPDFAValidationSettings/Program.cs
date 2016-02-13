d10cd9c51121bef74112c3571bb3b6c98d8ac8aa
apitron <support@apitron.com>
2016-02-13 15:12:45 +0400

    PDFA validation - fix the file contains cross reference stream

---
+++ UsingPDFAValidationSettings/Program.cs
@@ -0,0 +1,27 @@
+﻿using Apitron.PDF.Kit;
+using System.Diagnostics;
+using System.IO;
+
+namespace UsingPDFAValidationSettings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Stream stream = File.Open(@"../../data/document.pdf", FileMode.Open, FileAccess.Read))
+            {
+                // create document object and specify the output format
+                FixedDocument doc = new FixedDocument(stream, PdfStandard.PDFA);
+
+                // save document
+                using (Stream outputStream = File.Create(@"pdfa_document.pdf"))
+                {
+                    // turn off cross reference stream usage
+                    doc.IsCompressedStructure = false;
+                    doc.Save(outputStream);
+                }
+            }
+            Process.Start("pdfa_document.pdf");
+        }
+    }
+}
