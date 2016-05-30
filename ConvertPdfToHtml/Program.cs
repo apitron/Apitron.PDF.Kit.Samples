f49c1ec554a7bb04e5e3c2550f1139d2fdfc01aa
apitron <support@apitron.com>
2016-05-30 16:36:18 +0400

    convert PDF to HTML sample C# .NET

---
+++ ConvertPdfToHtml/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Apitron.PDF.Kit;
+using Apitron.PDF.Kit.Configuration;
+using Apitron.PDF.Kit.Extraction;
+
+namespace ConvertPdfToHtml
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // open pdf document
+            using (Stream inputStream = File.OpenRead("../../data/notification.pdf"))
+            {
+                using (FixedDocument doc = new FixedDocument(inputStream))
+                {               
+                    // create output file
+                    using (TextWriter writer = new StreamWriter(File.Create("out.html"),Encoding.UTF8))
+                    {
+                        // write returned html string to file
+                        writer.Write(doc.Pages[0].ConvertToHtml(TextExtractionOptions.HtmlPage));
+                    }
+                }
+            }
+
+            Process.Start("out.html");
+        }
+    }
+}
